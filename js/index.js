218a209260ea36c2355f58af66d5ecacbc109432
Sophia Nasseri <nasseri.sophia@gmail.com>
2021-11-12 17:48:19 +0100

    Fonctionnalité ouverture et fermeture dropdown et ajout de l'élément cliqué en tant que filtre sélectionné

--- js/index.js
+++ js/index.js
@@ -6,6 +6,14 @@ const recipesContainer = document.querySelector('.recipes');
 const ingredientListContainer = document.querySelector('#menu-ingredient');
 const applianceListContainer = document.querySelector('#menu-appliance');
 const utensilListContainer = document.querySelector('#menu-utensil');
+const dropdowns = document.querySelectorAll('.dropdown__toggle');
+const toggleLists = document.querySelectorAll('.toggle-list');
+const closeDropdownBtn = document.querySelectorAll('.close-dropdown');
+const applianceBtn = document.querySelector('#toggle-appliance');
+const utensilBtn = document.querySelector('#toggle-utensil');
+const ingredientToggle = document.querySelector('.toggle-list--ingredient');
+const applianceToggle = document.querySelector('.toggle-list--appliance');
+const filterResults = document.querySelector('.filter-results');
 
 const ingredientList = [];
 const applianceList = [];
@@ -13,7 +21,7 @@ const utensilList = [];
 
 const displayIngQuantity = (elt) => {
   const { ingredient, quantity, unit } = elt;
-  return `${ingredient} ${quantity ? `: ${quantity}` : ''} ${unit || ''}`;
+  return `<span class="bold">${ingredient}</span> ${quantity ? `: ${quantity}` : ''} ${unit || ''}`;
 };
 // Générer les cartes dynamiquement
 const recipesDisplay = () => {
@@ -45,35 +53,70 @@ const recipesDisplay = () => {
 };
 
 recipesDisplay();
-
-
-recipes.forEach((recipe) => {
-  recipe.ingredients.forEach((elt) => {
-    const ingredient = normalizeTag(elt.ingredient);
-    if (!ingredientList.includes(ingredient)) {
-      ingredientList.push(`<li>${ingredient}</li>`);
-      ingredientList.sort();
+// Générer le contenu des dropdowns dynamiquement
+const dropdownListDisplay = async () => {
+  recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((elt) => {
+      const ingredient = normalizeTag(elt.ingredient);
+      if (!ingredientList.includes(ingredient)) {
+        ingredientList.push(`<li class="option" data-type="ingredient">${ingredient}</li>`);
+        ingredientList.sort();
+      }
+    });
+    const appliance = normalizeTag(recipe.appliance);
+    if (!applianceList.includes(appliance)) {
+      applianceList.push(`<li class="option" data-type="appliance">${appliance}</li>`);
+      applianceList.sort();
     }
+    recipe.ustensils.forEach((elt) => {
+      const utensil = normalizeTag(elt);
+      if (!utensilList.includes(utensil)) {
+        utensilList.push(`<li class="option" data-type="utensil">${utensil}</li>`);
+        utensilList.sort();
+      }
+    });
   });
-  const appliance = normalizeTag(recipe.appliance);
-  if (!applianceList.includes(appliance)) {
-    applianceList.push(`<li>${appliance}</li>`);
-    applianceList.sort();
-  }
-  recipe.ustensils.forEach((elt) => {
-    const utensil = normalizeTag(elt);
-    if (!utensilList.includes(utensil)) {
-      utensilList.push(`<li>${utensil}</li>`);
-      utensilList.sort();
-    }
+};
+const dropdownResultDisplay = async () => {
+  const dropdownElements = document.querySelectorAll('.option');
+  dropdownElements.forEach((element) => {
+    element.addEventListener('click', (e) => {
+      closeDropdown();
+      const filterResultsDiv = document.createElement('div');
+      filterResults.appendChild(filterResultsDiv);
+      filterResultsDiv.classList.add('element-result');
+      const listContent = element.textContent;
+      const listDataset = e.target.dataset.type;
+      filterResultsDiv.setAttribute('data-type', listDataset);
+      filterResultsDiv.innerHTML = `
+        <p>${listContent}</p>
+        <button class="remove-result"><i class="far fa-times-circle"></i></button>
+      `;
+    });
+  });
+};
+const removeFilterResult = () => {
+  const removeResultBtn = document.querySelectorAll('.remove-result');
+  removeResultBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      console.log('hi');
+      console.log(e.target);
+    });
+  });
+};
 
+dropdownListDisplay().then(() => {
+  dropdownResultDisplay().then(() => {
+    removeFilterResult();
   });
 });
+
 const uniqueIngredientList = [...new Set(ingredientList)];
+
 ingredientListContainer.innerHTML = `
 
 ${uniqueIngredientList.join('')}
-`;
+ `;
 
 const uniqueApplianceList = [...new Set(applianceList)];
 applianceListContainer.innerHTML = `<ul>
@@ -87,7 +130,7 @@ ${uniqueUtensilList.join('')}
 
 /*
 const filterIngredientsList = recipes => {
-  const ingredientList = []; 
+  const ingredientList = [];
   recipes.forEach((recipe) => {
     recipe.ingredients.forEach((elt) => {
       const ingredient = normalizeTag(elt.ingredient);
@@ -99,3 +142,34 @@ const filterIngredientsList = recipes => {
   });
   return ingredientList
 } */
+
+// Dropdowns
+
+const closeDropdown = () => {
+  applianceBtn.classList.remove('margin-appliance');
+  utensilBtn.classList.remove('margin-utensil');
+  toggleLists.forEach((toggleList) => toggleList.classList.remove('active'));
+};
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener('click', (e) => {
+    toggleLists.forEach((toggleList) => {
+      const toggle = toggleList;
+      const toggleData = toggle.dataset.type;
+      const selectedToggle = toggleData.includes(e.target.dataset.type);
+      if (selectedToggle) {
+        toggle.classList.add('active');
+      } else if (ingredientToggle.classList.contains('active')) {
+        applianceBtn.classList.add('margin-appliance');
+      } else if (applianceToggle.classList.contains('active')) {
+        utensilBtn.classList.add('margin-utensil');
+      } else {
+        closeDropdown();
+      }
+    });
+  });
+});
+
+closeDropdownBtn.forEach((btn) => {
+  btn.addEventListener('click', closeDropdown);
+});
